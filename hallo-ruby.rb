0b9bb9028e3adc5bcfab230b7b6503d7e5199f38
Emile bosch <emilebosch@me.com>
2014-07-21 19:01:19 +0200

    Vandaag heb les 1 gehad

---
+++ hallo-ruby.rb
@@ -0,0 +1,23 @@
+
+# Hier laat ik de naam zien
+
+naam1="swannilda" # tekenreeks of tekst heet ook string -string staat altijd tussen "" qoutes
+naam2="emile"     # een variable staat aan de linkerkant - left side of the assignment
+
+# types kunnen zijn
+# string  = "<tekst komt hier>"
+# integer = 20 
+# float   = 23.4 = gebroken getal
+
+leeftijd=20 #leeftijd is een integer - getal 
+bedrag=23.5
+
+dubbele_leeftijd=leeftijd*leeftijd
+
+puts "met dubbele leeftijd ben je #{dubbele_leeftijd} jaar oud!"
+puts "jij bent #{leeftijd} jaar oud!"
+
+
+1.times do 
+  puts "Hallo #{naam1} en #{naam2}"
+end
